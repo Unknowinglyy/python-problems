e73ef4c8e4cc0dcc682272e668c3ab872ba9fe89
Unknowinglyy <unknowingly@tamu.edu>
2024-07-09 02:05:05 -0500

    roman numeral -> number

---
+++ roman2Num.py
@@ -0,0 +1,20 @@
+import sys
+
+value1 = sys.argv[1]
+
+romeDict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+curr = 0
+prev = 0
+total = 0
+for roman in value1:
+    #find the equivalent number of the roman numeral
+    curr = romeDict[roman]
+    #get the current total
+    total += curr
+    #if the current number is greater than the previous number, subtract the previous number twice from the total
+    if curr > prev:
+        total -= 2*prev
+    #set the previous number to the current number
+    prev = curr
+
+print(total)
